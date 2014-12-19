798fead5e034d28f96f9c20691b3c5b9babbe994
EileenLiu <contact.eileenliu@gmail.com>
2014-12-18 20:18:22 -0500

    Added PlayerPanel

---
+++ Battleship/core/src/com/mygdx/battleship/PlayerPanel.java
@@ -0,0 +1,21 @@
+package com.mygdx.battleship;
+
+import com.badlogic.gdx.scenes.scene2d.ui.Table;
+
+public class PlayerPanel extends Table {
+	AttackingPanel attackPanel;
+	PlacementPanel placementPanel;
+	public PlayerPanel(Grid myPlayer, Grid opponent, boolean disableAll){
+		attackPanel = new AttackingPanel(opponent);
+		placementPanel = new PlacementPanel(myPlayer);
+		this.add(attackPanel).pad(5).expand().fill();
+		this.row();
+		this.add(placementPanel).pad(5).expand().fill();
+		placementPanel.debug();
+	}
+/*	public void resize(int width, int height) {
+		System.out.println("playerPanel resize "+width +" , "+ height);
+		attackPanel.resize(width, height/2);
+		placementPanel.resize(width, height/2);
+	}*/
+}
